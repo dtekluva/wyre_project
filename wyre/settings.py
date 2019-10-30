e23376f6e8a16908aa318d0d6aced721e5b30b80
Inyang Kpongette <31896598+dtekluva@users.noreply.github.com>
2019-10-30 15:41:42 +0100

    allowed debug

--- wyre/settings.py
+++ wyre/settings.py
@@ -40,7 +40,8 @@ CACHE_EXPIRY = 1800
 SECRET_KEY = 'hdhw*b6jko0m!@8@j8ufk+1ybj8u#gc@4ov0_xec((exhar=io'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if HOSTNAME == "DESKTOP-U00EROM" else False
+# DEBUG = True if HOSTNAME == "DESKTOP-U00EROM" else False
+DEBUG = True
 
 ALLOWED_HOSTS = ['wyre.pythonanywhere.com',
                 'localhost'
