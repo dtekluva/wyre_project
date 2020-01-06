ca23fc4193a82f0930b95eba536d0ccdc3057c44
Inyang Kpongette <31896598+dtekluva@users.noreply.github.com>
2020-01-06 12:25:53 +0100

    minor tweak

--- wyre/settings.py
+++ wyre/settings.py
@@ -81,10 +81,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'wyre.urls'
 
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     '/var/www/static/',
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 TEMPLATES = [
     {
