fb0abb9d7666023c2840e8af64380dbab87809a5
Inyang Kpongette <31896598+dtekluva@users.noreply.github.com>
2020-01-07 02:20:34 +0100

    added cron job

---
+++ main/management/commands/update.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from main.models import *
+from main.helpers.fetch_readings import run_migrations
+
+
+try:
+
+    run_migrations()
+except:
+    pass
+
+for i in range(5):
+
+    Datalog().populate()       
+
+
+# def update_historic_scores(request):
+
+#         devices = Device.objects.all()                        
+
+#         for device in devices:
+#                 device.populate_previous_scores()
